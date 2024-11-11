5947f9fad2a180d4ef395fa7bdbbd7633b78f8be
SadeceDeniz <152819541+SadeceDeniz@users.noreply.github.com>
2024-11-11 23:51:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Sizinle Uğraşıyorumm", "Partner yapalım mı?", "discord.gg/bushi", "yardım için wistyyx", "yardım için blackeker", "kuru kuru kururin", "Gambare Gambare!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
